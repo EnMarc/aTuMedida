16df390ce92c44564588611131c3683ab21e428f
Enrique Marenco <wilmer_marenco@hotmail.com>
2018-09-10 22:28:58 -0600

    Initial commit

---
+++ src/main/java/sv/efe/atm/ATuMedidaApplication.java
@@ -0,0 +1,12 @@
+package sv.efe.atm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ATuMedidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ATuMedidaApplication.class, args);
+	}
+}
---
+++ src/test/java/sv/efe/atm/ATuMedidaApplicationTests.java
@@ -0,0 +1,16 @@
+package sv.efe.atm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ATuMedidaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
